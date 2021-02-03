c4c0af47960a3f9f3db054f0c3e1d9d044a2a45a
Azat Yılmaz <Azat.Ylmz@hotmail.com>
2021-02-03 23:46:53 +0300

    Add project files.

---
+++ Entities/Concrate/Product.cs
@@ -0,0 +1,19 @@
+﻿using Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.Concrate
+{
+    //internal eklemek = sadece çalıştığın katman ulaşabilsin demek
+    //public eklemek = bu klasa diğer katmanlarda ulaşabilsin demek
+    public class Product : IEntity
+    {
+        public int ProductId { get; set; }
+        public int CategoryId { get; set; }
+        public string ProductName { get; set; }
+        public short UnitsInStock { get; set; }
+        public decimal UnitPrice { get; set; }
+
+    }
+}
