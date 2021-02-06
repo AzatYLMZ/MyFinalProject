229dd1106d1cc98c8fae75313194cb42587435e4
Azat Yılmaz <Azat.Ylmz@hotmail.com>
2021-02-06 23:56:30 +0300

    06.02.2021

--- DataAccess/Concrate/EntityFramework/EfOrderDal.cs
+++ DataAccess/Concrate/EntityFramework/EfOrderDal.cs
@@ -3,13 +3,11 @@ using DataAccess.Abstract;
 using Entities.Concrate;
 using System;
 using System.Collections.Generic;
-using System.Linq.Expressions;
 using System.Text;
 
 namespace DataAccess.Concrate.EntityFramework
 {
-    public class EfCategoryDal : EfEntityRepositoryBase<Category, NorthwindContext>, ICategoryDal
+    public class EfOrderDal : EfEntityRepositoryBase<Order, NorthwindContext>,IOrderDal
     {
-        
     }
 }
